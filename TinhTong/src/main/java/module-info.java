d9e67caa6612bd2160e3d5a883b82497f166f67a
Xuân Trường <157230320+Truongdoxuan@users.noreply.github.com>
2024-10-08 13:57:50 +0700

    Delete TinhTong directory

--- TinhTong/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.tinhtong {
-    requires javafx.controls;
-    requires javafx.fxml;
-  requires java.desktop;
-
-
-  opens com.example.tinhtong to javafx.fxml;
-    exports com.example.tinhtong;
-}
