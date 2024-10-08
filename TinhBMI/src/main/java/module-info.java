7ad8991e73ce6882002ff5b49da145fa5b5d314e
Xuân Trường <157230320+Truongdoxuan@users.noreply.github.com>
2024-10-08 13:57:29 +0700

    Delete TinhBMI directory

--- TinhBMI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.tinhbmi {
-  requires javafx.controls;
-  requires javafx.fxml;
-  requires java.desktop;
-
-
-  opens com.example.tinhbmi to javafx.fxml;
-  exports com.example.tinhbmi;
-}
