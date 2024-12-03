944441b77521384f734afd4a530851d6bcc649cb
Xuân Trường <dxt8104@gmail.com>
2024-12-03 15:29:31 +0700

    bai2 tinhBmi completed

--- Bai2_BasicGUI_BMI/app/src/main/java/th/doxuantruong/bmi/MainActivity.java
+++ Bai2_BasicGUI_BMI/app/src/main/java/th/doxuantruong/bmi/MainActivity.java
@@ -1,6 +1,7 @@
 package th.doxuantruong.bmi;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -13,9 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     //khai báo biến
-    TextView chieucao, cannang, ketqua;
+    TextView chieucao, cannang, ketqua, nhanxet;
     CheckBox isChauA;
-    Button tinhbmi;
+    Button tinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,54 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        
+        chieucao = findViewById(R.id.txt_chieucao);
+        cannang = findViewById(R.id.txt_cannang);
+        isChauA = findViewById(R.id.cb_asian);
+        tinh = findViewById(R.id.btn_tinh);
+        ketqua = findViewById(R.id.kqua);
+        nhanxet = findViewById(R.id.nxet);
+
+        //khi nhấn btn tính
+        tinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tinhBMI();
+            }
+        });
+    }
+    //hàm thực hiện tính
+    public void tinhBMI() {
+        double ccao = Double.parseDouble(chieucao.getText().toString());
+        double cnang = Double.parseDouble(cannang.getText().toString());
+        boolean isAsia = isChauA.isChecked();
+        String cmt;
+
+        //cth tính
+        double bmi = cnang / (ccao * ccao);
+        ketqua.setText(String.format("%.2f",bmi));
+
+        //nhận xét
+        if (isAsia) {
+            if (bmi < 18.5) {
+                cmt = "Gầy";
+            } else if (bmi < 23) {
+                cmt = "Bình thường";
+            } else if (bmi < 27.5) {
+                cmt = "Thừa cân";
+            } else {
+                cmt = "Béo phì";
+            }
+        } else {
+            if (bmi < 18.5) {
+                cmt = "Gầy";
+            } else if (bmi < 23) {
+                cmt = "Bình thường";
+            } else if (bmi < 30) {
+                cmt = "Thừa cân";
+            } else {
+                cmt = "Béo phì";
+            }
+        }
+        nhanxet.setText(cmt);
     }
 }
